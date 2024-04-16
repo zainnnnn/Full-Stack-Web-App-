1ef2e308c5cea1ea3e1bedc1f5b08493cdfec4b0
zainnnnn <mzain2353@gmail.com>
2024-04-16 14:58:07 +0200

    Create custom model and views for Notes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 class UserSerializer (serializers.ModelSerializer):
@@ -9,5 +10,13 @@ class UserSerializer (serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
+        print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = "__all__"
+        extra_kwargs = {"author": {"read_only": True}}
