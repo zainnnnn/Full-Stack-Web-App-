2630b226fd7a6428071c999639e89c2298a40203
zainnnnn <mzain2353@gmail.com>
2024-04-21 23:39:28 +0200

    Added gitignore file

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("notes/", view=views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>",
+    path("notes/delete/<int:pk>/",
          view=views.NoteDelete.as_view(), name="delete-note"),
 
 ]
